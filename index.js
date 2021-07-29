0568b31274e87295135375e5a3620a49a67b1bbb
Raelinda Woad <raelindawoad@comcast.net>
2021-07-29 17:33:23 -0400

    added js

---
+++ index.js
@@ -0,0 +1,35 @@
+let ldSurvey = document.getElementById('ld-survey')
+
+ldSurvey.addEventListener('submit', e => {
+    event.preventDefault
+    let ldFormData = new FormData(event.target)
+    let firstName = ldFormData.get('fName')
+    let messageToUser = `<h2>Hey there, ${firstName}!<br>
+    This is a student project and not a real survey form.<br>
+    However...<br>
+    If you are interested in learning how to lucid dream I recommend watching Michael Raduga's conscious dreaming instructional videos. They were recorded in Russia and translated into english.</h2>
+   <a class='raduga' href="https://obe4u.com" target='_blank'>obe4u.com</a>
+    <h2>(Don't let Raduga's initial woo-woo syntax throw you off. He delivers pragmatic, field-tested LD induction techniques.)</h2>`
+    let mainContent = document.getElementById('main-content')
+    mainContent.classList.add('messageToUser')
+    mainContent.innerHTML = messageToUser
+})
+
+
+
+
+
+
+// const emailCollectorForm = document.getElementById('email-collector')
+
+// emailCollectorForm.addEventListener('submit', e => {
+//     event.preventDefault
+//     let emailFormData = new FormData(event.target)
+// let userFirstName = emailFormData.get('firstName')
+// let userEmailAddress = emailFormData.get('emailAddress')
+// let updatedMainContent = `<h2>Congratulations, ${userFirstName}!</h2>
+// <p>You're BBQ journey has begun!</p>
+// <p class="fine-print">updates will be sent to ${userEmailAddress}</p>`
+// let mainContent = document.getElementById('mainContent')
+// mainContent.innerHTML = updatedMainContent
+// })
